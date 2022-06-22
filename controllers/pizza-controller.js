7f2d468e517478a8dd898be12c8f2b0f350cf603
Alex Vazquez <avazque008@outlook.com>
2022-06-22 00:18:18 -0400

    comment-model created, as well as its routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,7 +3,12 @@ const { Pizza } = require('../models');
 const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
-    Pizza.find({})
+    Pizza.find({}).populate({
+      path: 'comments',
+      select: '-__v'
+    })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -14,10 +19,21 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
